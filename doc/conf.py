629ebdd78e8ac517c815a1b2db9a20e1f8589ba4
Carlos Aznarán <caznaranl@uni.pe>
2025-03-21 22:15:02 -0500

    Ignore error

--- doc/conf.py
+++ doc/conf.py
@@ -43,3 +43,6 @@ html_theme = "sphinx_rtd_theme"
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
+
+# You can ignore this error by setting the following in conf.py
+nbsphinx_allow_errors = True
